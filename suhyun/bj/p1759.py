7862effeb0b5f933306e6ae9727cf46959cc66e6
suhyun <qkrtngus116@naver.com>
2024-01-19 14:52:35 +0900

    feat: 완전탐색

---
+++ suhyun/bj/p1759.py
@@ -0,0 +1,30 @@
+import sys
+m, n = tuple(map(int, sys.stdin.readline().split()))
+chars = sorted(input().split(' '))
+used = [0] * n
+selected = [0] * m
+
+def is_vowel(x: str):
+    return x in "aeiou"
+
+def rec_func(k):
+    if k == m:
+        vowel, consonant = 0, 0
+        for x in selected:
+            if is_vowel(chars[x]):
+                vowel += 1
+            else:
+                consonant += 1
+        
+        if (vowel >= 1) and (consonant >= 2):
+            for x in selected:
+                print(chars[x], end='')
+            print()
+    
+    else:
+        st = -1 if k == 0 else selected[k - 1]
+        for i in range(st + 1, n):
+            selected[k] = i
+            rec_func(k + 1)
+
+rec_func(0)
