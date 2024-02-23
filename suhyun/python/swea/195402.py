b98765f1bf179e1045831573f76080807afb25a0
suhyun <qkrtngus116@naver.com>
2024-02-23 14:14:25 +0900

    feat: 구현

--- suhyun/python/swea/195402.py
+++ suhyun/python/swea/195402.py
@@ -1,13 +1,13 @@
 t = int(input())
+dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 for tc in range(1, t+1):
     n = int(input())
-    dist = [[0, 1], [1, 0], [0, -1], [-1, 0]]
     arr = [[0] * n for _ in range(n)]
-    num = 1 # 달팽이를 채울 숫자
-    d = 0 # 달팽이 이동방향
+    num = 1
     x, y = 0, -1
-    while num <= (n*n):
-        nx, ny = x + dist[d][0], y + dist[d][1]
+    d = 0
+    while num <= (n * n):
+        nx, ny = x + dir[d][0], y + dir[d][1]
         if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 0:
             arr[nx][ny] = num
             num += 1
