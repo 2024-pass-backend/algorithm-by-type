a722b64d213436eacae634786a51a3ed97e2f9c5
suhyun <qkrtngus116@naver.com>
2024-02-27 09:22:42 +0900

    feat: 완전탐색

---
+++ suhyun/python/bj/p1565106.py
@@ -0,0 +1,16 @@
+def pro(x):
+    if x == m:
+        for z in selected:
+            sys.stdout.write(str(z) + ' ')
+        sys.stdout.write('\n')
+    
+    else:
+        for cand in range(1, n+1):
+            selected[x] = cand
+            pro(x + 1)
+
+import sys
+n, m = tuple(map(int, input().split()))
+selected = [0 for _ in range(m)]
+pro(0)
+    
