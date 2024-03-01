05b447d6cee0048a3b3dd11ce5b5ff9301f6152d
suhyun <qkrtngus116@naver.com>
2024-03-01 16:19:01 +0900

    feat: 구현

--- suhyun/python/pg/solution02.py
+++ suhyun/python/pg/solution02.py
@@ -1,18 +1,31 @@
 # 대충 만든 자판 (in, not in 키워드)
-keymap = ["ABACD", "BCEFD"]
-targets = ["ABCD","AABB"]
+keymap = ["BC"]
+targets = ["AC", "BC"]
 
 def solution(keymap, targets):
     answer = []
     
-    for word in targets: #"ABCD", "AABB"
-        cnt = 0
+    for word in targets: #"AC", "BC"
+        sum_cnt = 0
+        ss = False
         for word_elem in word:
             min_cnt = 101
-            for elem in keymap:     # elem = "ABACD"
+            for elem in keymap:     # keymap = "BC", word_elem "A"
                 if word_elem not in elem:
                     continue
-                min_cnt = min(elem.index(word_elem), min_cnt)
-            if min_cnt == 101: 
-                cnt = -1
-        answer.append(cnt)
+                min_cnt = min(elem.index(word_elem) + 1, min_cnt)
+                if min_cnt == 101:
+                    ss = True
+                    break
+            if min_cnt == 101:
+                ss = True
+                min_cnt = -1
+                sum_cnt = 101
+                break
+            sum_cnt += min_cnt
+        if sum_cnt == 101:
+            sum_cnt = -1
+        answer.append(sum_cnt)
+    return answer
+
+solution(keymap, targets)
